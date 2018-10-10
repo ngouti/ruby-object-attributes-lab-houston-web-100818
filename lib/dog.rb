c667a69a2f58a2e80446e4e566adc4da878a2df1
ngouti <gouti.naama@gmail.com>
2018-10-10 18:43:57 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+
+
+    def name
+        @name
+    end
+
+    def name=(dogName)
+        @name = dogName
+    end
+
+    def breed
+        @breed
+    end
+
+    def breed=(dogBre)
+        @breed = dogBre
+    end
+
+    
+end
+
---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person
+
+
+    def name
+        @name
+    end
+
+    def name=(perName)
+        @name = perName
+    end
+
+    def job
+        @job
+    end
+
+    def job=(perJob)
+        @job = perJob
+
+    end
+
+    
+end
+
